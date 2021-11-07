24d61e7d91c942167f73c141f01d3d98b96d986b
Suyogya Shrivastava <suyogya.ug20@nsut.ac.in>
2021-11-07 23:26:29 +0530

    Header

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,11 @@
- 
+import React from "react";
+import Headers from './components/header'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Headers/>
+     
     </div>
   );
 }
