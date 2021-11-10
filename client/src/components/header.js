c030d2629cb482f9f443cbd375b17de12d34ed9b
Suyogya Shrivastava <suyogya.ug20@nsut.ac.in>
2021-11-10 23:49:45 +0530

    Navbar modified+ Left section

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,18 +1,32 @@
 import React from 'react'
-
+import {Link} from "react-router-dom";
 const headers = () => {
+  const btnn={
+    borderRadius:0,
+  }
+
     return (
         <>
         <div>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light mx-2 my-1 px-2 py-2">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Code-ite</a>
+    <Link className="navbar-brand mb-0 h1 " to="#">{"</>"}</Link>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <Link class="nav-link active mx-5 my-1 h5" aria-current="page" to="#">Support</Link>
+        </li>
+        <li class="nav-item">
+          <Link class="nav-link active mx-6 my-1 h5" to="#">About</Link>
+        </li>
+        </ul>
+        </div>
     
      
     <div className="  navbar- " id="navbarSupportedContent">
-    <button type="button" class="btn btn-outline-secondary mx-2">Sign Up</button>
+    <button type="button" class="btn btn-outline-dark text-nowrap px-4 py-2 mx-4 "style={btnn}>Login</button>
      
-      <button type="button" class="btn btn-outline-secondary">Login</button>
+      <button type="button" class="btn btn-dark px-4 py-2 text-nowrap mx-1" style={btnn}>Sign Up</button>
     </div>
   </div>
 </nav>
